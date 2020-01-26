39c31f875658409e5438eb05d4137799a15fffce
Esmaeil Ahmadipour <software8686@gmail.com>
2020-01-26 23:54:10 +0330

    Commit> Bottom Navigation View > Show Fragments In FrameLayout

--- app/src/main/java/ir/ea2/android_materialdesign/FourActivity.java
+++ app/src/main/java/ir/ea2/android_materialdesign/FourActivity.java
@@ -10,6 +10,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import ir.ea2.android_materialdesign.bottom_navigation.HomeFragment;
+import ir.ea2.android_materialdesign.bottom_navigation.SearchFragment;
+import ir.ea2.android_materialdesign.bottom_navigation.SettingsFragment;
+import ir.ea2.android_materialdesign.bottom_navigation.Test;
 
 public class FourActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -21,6 +26,14 @@ public class FourActivity extends AppCompatActivity {
         setContentView(R.layout.activity_four);
         setupToolbar();
         setupBottomNavigationView();
+        loadFragments(new HomeFragment());
+    }
+
+    private void loadFragments(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.ac_activity4_fragment_container, fragment)
+                .commit();
+        System.out.println(fragment.isStateSaved());
     }
 
     private void setupBottomNavigationView() {
@@ -33,16 +46,21 @@ public class FourActivity extends AppCompatActivity {
                     case R.id.bottom_nav_home: {
                         Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_home), Toast.LENGTH_SHORT)
                                 .show();
+                        loadFragments(new HomeFragment());
+                        System.out.println(new Test(5));
+
                         return true;
                     }
                     case R.id.bottom_nav_search: {
                         Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_search), Toast.LENGTH_SHORT)
                                 .show();
+                        loadFragments(new SearchFragment());
                         return true;
                     }
                     case R.id.bottom_nav_settings: {
                         Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_settings), Toast.LENGTH_SHORT)
                                 .show();
+                        loadFragments(new SettingsFragment());
                         return true;
                     }
                 }
---
+++ app/src/main/java/ir/ea2/android_materialdesign/bottom_navigation/Test.java
@@ -0,0 +1,17 @@
+package ir.ea2.android_materialdesign.bottom_navigation;
+
+public class Test {
+    int x ;
+
+    public Test(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
