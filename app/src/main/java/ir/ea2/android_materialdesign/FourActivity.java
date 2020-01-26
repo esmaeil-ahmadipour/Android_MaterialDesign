e43bc36856ab0ad4cc4ab073406921c908fec469
Esmaeil Ahmadipour <software8686@gmail.com>
2020-01-26 22:43:00 +0330

    Commit> Bottom Navigation View > Add onNavigationItemSelectedListener

--- app/src/main/java/ir/ea2/android_materialdesign/FourActivity.java
+++ app/src/main/java/ir/ea2/android_materialdesign/FourActivity.java
@@ -1,20 +1,54 @@
 package ir.ea2.android_materialdesign;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Color;
-import android.os.Bundle;
-
 public class FourActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four);
         setupToolbar();
+        setupBottomNavigationView();
+    }
 
+    private void setupBottomNavigationView() {
+        bottomNavigationView = findViewById(R.id.ac_activity4_bnv);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                switch (itemId) {
+                    case R.id.bottom_nav_home: {
+                        Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_home), Toast.LENGTH_SHORT)
+                                .show();
+                        return true;
+                    }
+                    case R.id.bottom_nav_search: {
+                        Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_search), Toast.LENGTH_SHORT)
+                                .show();
+                        return true;
+                    }
+                    case R.id.bottom_nav_settings: {
+                        Toast.makeText(FourActivity.this, getResources().getString(R.string.nav_settings), Toast.LENGTH_SHORT)
+                                .show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     private void setupToolbar() {
