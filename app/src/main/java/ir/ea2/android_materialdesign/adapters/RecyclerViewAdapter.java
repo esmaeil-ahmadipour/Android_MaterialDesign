99516a8730ef3321e51f15150330deee8f977d16
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-01 14:51:22 +0330

    Commit> RecyclerView > Changing Layout Background (Order By Even or Odd Position)

--- app/src/main/java/ir/ea2/android_materialdesign/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/ir/ea2/android_materialdesign/adapters/RecyclerViewAdapter.java
@@ -1,10 +1,12 @@
 package ir.ea2.android_materialdesign.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import java.util.List;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import ir.ea2.android_materialdesign.R;
 import ir.ea2.android_materialdesign.models.MessageModel;
@@ -21,7 +23,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<MessageViewHolder>
     @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_eight_item, parent, false);
+                .inflate(R.layout.activity_eight_item2, parent, false);
         return new MessageViewHolder(view);
     }
 
@@ -29,7 +31,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<MessageViewHolder>
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
         holder.getAvatar().setImageResource(messageModelList.get(position).getAvatarItem());
         holder.getName().setText(messageModelList.get(position).getNameItem());
+        setViewBackgroundColor(holder.getView(),position);
         holder.getLastMessage().setText(messageModelList.get(position).getLastMessageItem());
+    }
+
+    private void setViewBackgroundColor(View view, int position) {
+        if((position+1)%2 == 0){
+            view.setBackgroundColor(ContextCompat.getColor(view.getContext(),R.color.colorGray));
+        }else{
+            view.setBackgroundColor(ContextCompat.getColor(view.getContext(),R.color.colorWhite));
+
+        }
     }
 
     @Override
