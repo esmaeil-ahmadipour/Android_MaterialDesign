e977cfc8f25cbfc6e730b6841422c77ca50fd12e
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-03 04:38:51 +0330

    Commit> Customized CollapsingToolbarLayout > Set Parent-Activity / Sub-Activity

--- app/src/main/java/ir/ea2/android_materialdesign/DetailEightActivity.java
+++ app/src/main/java/ir/ea2/android_materialdesign/DetailEightActivity.java
@@ -2,10 +2,12 @@ package ir.ea2.android_materialdesign;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import ir.ea2.android_materialdesign.models.MessageModel;
@@ -27,6 +29,19 @@ public class DetailEightActivity extends AppCompatActivity {
         bindData();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case(android.R.id.home):{
+                Toast.makeText(this, name.getText().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void bindData() {
         MessageModel messageModel = (MessageModel) getIntent().getExtras().getSerializable(USER_KEY);
         avatar.setImageResource(messageModel.getAvatarItem());
@@ -44,5 +59,7 @@ public class DetailEightActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
